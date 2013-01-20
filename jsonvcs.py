c2e2d15ba4dbc18195a609944afa31a45ba9ef88
Sean Lang <slang800@gmail.com>
2013-01-19 18:29:05 -0600

    changed to standard hash function... old one assumed immutability

--- jsonvcs.py
+++ jsonvcs.py
@@ -19,8 +19,6 @@ class Doc(collections.Mapping):
 		if 'current' not in self.doc:
 			self.doc['current'] = {}
 
-		self._hash = None
-
 	def __getitem__(self, key):
 		return self.doc[key]
 
@@ -31,16 +29,7 @@ class Doc(collections.Mapping):
 		return len(self.doc)
 
 	def __hash__(self):
-		# It would have been simpler and maybe more obvious to use
-		# hash(tuple(sorted(self._d.iteritems()))) from this discussion so
-		# far, but this solution is O(n). I don't know what kind of n we are
-		# going to run into, but sometimes it's hard to resist the urge to
-		# optimize when it will gain improved algorithmic performance.
-		if self._hash is None:
-			self._hash = 0
-			for pair in self.iteritems():
-				self._hash ^= hash(pair)
-		return self._hash
+		return hash(self.doc)
 
 	def __repr__(self):
 		"""just output the json representation of the current doc"""
